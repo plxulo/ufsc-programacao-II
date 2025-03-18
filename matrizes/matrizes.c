e0b40ea215d6415d8855956826d0b8780320d48d
Paulo <pcfdaa@gmail.com>
2025-03-17 23:39:39 -0300

    matriz: fixed error in dec function where a < 0

--- matrizes/matrizes.c
+++ matrizes/matrizes.c
@@ -35,7 +35,7 @@ void inc(int matrix[MAX_N][MAX_N], int N) {
 void dec(int matrix[MAX_N][MAX_N], int N) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      matrix[i][j] = (matrix[i][j] - 1) % 10;
+      matrix[i][j] = (matrix[i][j] - 1 + 10) % 10;
     }
   }
 }
